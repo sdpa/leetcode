a829f85c73cf1e803c7e07436260be6c4fdd13ca
Sai Patibandla <sdpa14@gmail.com>
2026-03-31 21:29:29 -0500

    Time: 2 ms (45.34%) | Memory: 19.1 MB (95.42%) - LeetSync

---
+++ 71-simplify-path/simplify-path.py
@@ -0,0 +1,31 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        
+        dir_stack = []
+
+        elements = path.split("/")
+
+        aboslute_path = []
+
+        for element in elements:
+            if len(element) == 0:
+                continue
+            
+            if element == "..":  # Go up the root directory.
+                if len(dir_stack) > 0:
+                    dir_stack.pop(-1)
+
+            elif element == ".": # We stay in the current directory. we dont do anything. 
+                continue
+            else: # Valid name of a directory. 
+                dir_stack.append(element)
+
+        # End, we join the dir_stack. 
+
+        res = "/" + "/".join(dir_stack)
+
+        print(res)
+
+        return res
+
+        
